47bfeadae857a9ad6e116a9d38a5faf76c56e965
James <james@jan.ai>
2023-12-29 16:58:35 +0700

    fix(JanHub): #1158 sort model list
    Signed-off-by: James <james@jan.ai>

--- web/screens/ExploreModels/ExploreModelList/index.tsx
+++ web/screens/ExploreModels/ExploreModelList/index.tsx
@@ -8,33 +8,30 @@ type Props = {
 }
 
 const ExploreModelList: React.FC<Props> = ({ models }) => {
-  const sortOrder: Record<string, number> = {
-    '7b': 1,
-    '13b': 2,
-    '34b': 3,
-    '70b': 4,
-    '120b': 5,
-    'tiny': 6,
-  }
-  const sortedModels = models?.sort((a, b) => {
-    const aIsFeatured = a.metadata.tags.includes('Featured')
-    const bIsFeatured = b.metadata.tags.includes('Featured')
-    const aIsRecommended = a.metadata.tags.includes('Recommended')
-    const bIsRecommended = b.metadata.tags.includes('Recommended')
-    const aNumericTag =
-      a.metadata.tags.find((tag) => !!sortOrder[tag.toLowerCase()]) ?? 'Tiny'
-    const bNumericTag =
-      b.metadata.tags.find((tag) => !!sortOrder[tag.toLowerCase()]) ?? 'Tiny'
+  const sortedModels = models?.sort((m1, m2) => {
+    const aIsFeatured = m1.metadata.tags.includes('Featured')
+    const bIsFeatured = m2.metadata.tags.includes('Featured')
+    const aIsRecommended = m1.metadata.tags.includes('Recommended')
+    const bIsRecommended = m2.metadata.tags.includes('Recommended')
 
-    if (aIsFeatured !== bIsFeatured) return aIsFeatured ? -1 : 1
-    if (aNumericTag !== bNumericTag)
-      return (
-        sortOrder[aNumericTag.toLowerCase()] -
-        sortOrder[bNumericTag.toLowerCase()]
-      )
-    if (aIsRecommended !== bIsRecommended) return aIsRecommended ? -1 : 1
-    return a.metadata.size - b.metadata.size
+    if (aIsFeatured !== bIsFeatured) {
+      return aIsFeatured ? -1 : 1
+    } else if (aIsFeatured) {
+      // Both are featured
+      return m1.metadata.size - m2.metadata.size
+    }
+
+    if (aIsRecommended !== bIsRecommended) {
+      return aIsRecommended ? -1 : 1
+    } else if (aIsRecommended) {
+      // Both are recommended
+      return m1.metadata.size - m2.metadata.size
+    }
+
+    // Neither featured nor recommended
+    return m1.metadata.size - m2.metadata.size
   })
+
   return (
     <div className="relative h-full w-full flex-shrink-0">
       {sortedModels?.map((model) => (
